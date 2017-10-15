c797a6e7bfc8c9ba85e433bc4f8a09bca46e6f05
Kishan <patelk6@students.rowan.edu>
2017-10-15 17:34:07 -0400

    Code for milestone

--- code/main.c
+++ code/main.c
@@ -1,47 +1,179 @@
-#include <msp430F5529.h>
-unsigned int delay=0;
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2015, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP430 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP430 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
+ * for an API functional library-approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//  MSP430FR5x9x Demo - eUSCI_A0 UART echo at 9600 baud using BRCLK = 8MHz
+//
+//  Description: This demo echoes back characters received via a PC serial port.
+//  SMCLK/ DCO is used as a clock source and the device is put in LPM3
+//  The auto-clock enable feature is used by the eUSCI and SMCLK is turned off
+//  when the UART is idle and turned on when a receive edge is detected.
+//  Note that level shifter hardware is needed to shift between RS232 and MSP
+//  voltage levels.
+//
+//  The example code shows proper initialization of registers
+//  and interrupts to receive and transmit data.
+//  To test code in LPM3, disconnect the debugger.
+//
+//  ACLK = VLO, MCLK =  DCO = SMCLK = 8MHz
+//
+//                MSP430FR5994
+//             -----------------
+//       RST -|     P2.0/UCA0TXD|----> PC (echo)
+//            |                 |
+//            |                 |
+//            |     P2.1/UCA0RXD|<---- PC
+//            |                 |
+//
+//   William Goh
+//   Texas Instruments Inc.
+//   October 2015
+//   Built with IAR Embedded Workbench V6.30 & Code Composer Studio V6.1
+//******************************************************************************
+#include <msp430FR5994.h>
+unsigned int totalBytes = 0;
+unsigned int byteCount = 0;
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    P3SEL0 |= BIT4 + BIT5 +BIT6;
+    P3DIR|= BIT4 + BIT5 +BIT6;
 
-
-       P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-       P1DIR |= BIT0;                          // Set P1.0 to output direction
-
-       //choosing pin 2.0 for hardware pwm
-       P2SEL |= BIT0;
-       P2DIR|= BIT0;
-
-       TA1CCTL1 += OUTMOD_7;
-       TA1CTL = TASSEL_2 + MC_1;
-       TA1CCR0 = 1000;
-       TA1CCR1 = 100;
+      TB0CCTL3 += OUTMOD_7;
+      TB0CCTL4 += OUTMOD_7;
+      TB0CCTL5 += OUTMOD_7;
+      TB0CTL = TASSEL_2 + MC_1;
+      TB0CCR0 = 255;
 
 
 
-       P1DIR &= ~BIT1;
-       P1OUT |= BIT1;
-       P1REN |= BIT1;
+    WDTCTL = WDTPW | WDTHOLD;               // Stop Watchdog
 
-    //   TA0CCTL0 = CCIE;                          // CCR0 interrupt enabled
-     //  TA0CCR0 = 62500;
-     //  TA0CTL = TASSEL_1 + MC_1 + ID_3;         // SMCLK, upmode, clear TAR
+    // Configure GPIO
+    P2SEL0 &= ~(BIT0 | BIT1);
+    P2SEL1 |= BIT0 | BIT1;                  // USCI_A0 UART operation
+
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;
+
+    // Startup clock system with max DCO setting ~8MHz
+    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+    CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
+    CSCTL0_H = 0;                           // Lock CS registers
+
+    // Configure USCI_A0 for UART mode
+    UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+    // Baud Rate calculation
+    // 8000000/(16*9600) = 52.083
+    // Fractional portion = 0.083
+    // User's Guide Table 21-4: UCBRSx = 0x04
+    // UCBRFx = int ( (52.083-52)*16) = 1
+    UCA0BRW = 52;                           // 8000000/16/9600
+    UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+    UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+    UCA0IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
+
+    __bis_SR_register(LPM3_bits | GIE);     // Enter LPM3, interrupts enabled
+    __no_operation();                       // For debugger
+}
+
+void setPWMDuty(int duty, int color)
+{
+    switch(color){
+        case 1:
+            TB0CCR3 = duty;
+            break;
+        case 2:
+            TB0CCR4 = duty;
+            break;
+        case 3:
+            TB0CCR5 = duty;
+            break;
+    }
 
 
 
-    //   __bis_SR_register(LPM0_bits + GIE);
-     //  __no_operation();
-       while(1){
-
-       }
-   }
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void clockOverFlow(void){
-
-    P1OUT^=BIT0;
 
 }
 
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=EUSCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(EUSCI_A0_VECTOR))) USCI_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
+    {
+        case USCI_NONE: break;
+        case USCI_UART_UCRXIFG:
+            while(!(UCA0IFG&UCTXIFG));
+            if(byteCount == 0){
+                totalBytes = UCA0RXBUF;
+                UCA0TXBUF = UCA0RXBUF - 0x03;
+                byteCount++;
+            }else if(byteCount<4){
+                setPWMDuty(UCA0RXBUF,byteCount);
+                byteCount++;
+            }else{
+                UCA0TXBUF = UCA0RXBUF;
+                byteCount++;
+            }
+            if(byteCount == totalBytes-4){
+                byteCount = 0;
+            }
 
-
-
+            __no_operation();
+            break;
+        case USCI_UART_UCTXIFG: break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
+        default: break;
+    }
+}
